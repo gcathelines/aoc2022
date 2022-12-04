f3df4b6b9fa6d584c60a7c7cb906d70988603bf5
Gabriellyan Catheline <gabriellyan@pintu.co.id>
2022-12-04 12:34:32 +0700

    day 4

---
+++ 4.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("./4.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	count1 := 0
+	count2 := 0
+
+	for scanner.Scan() {
+		txt := scanner.Text()
+		var a1, a2, b1, b2 int
+
+		if _, err := fmt.Sscanf(txt, "%d-%d,%d-%d", &a1, &a2, &b1, &b2); err != nil {
+			fmt.Println("error", err)
+		}
+
+		if a1 <= b1 && a2 >= b2 || a1 >= b1 && a2 <= b2 {
+			count1++
+		}
+
+		if a1 >= b1 && a1 <= b2 {
+			count2++
+			continue
+		}
+
+		if b1 >= a1 && b1 <= a2 {
+			count2++
+			continue
+		}
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	fmt.Println("Prob 1:", count1, "Prob 2:", count2)
+
+}
